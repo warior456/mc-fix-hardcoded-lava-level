2c7e57cb68e5e55164adac7d1f897134ac3bb2a2
mrburger <nicklekson@gmail.com>
2026-08-21 15:03:44 -0400

    Updated Version to 1.3.0 - Fabric API required now (datapack parsing). Built against Minecraft 26.2 Added the feature to use a custom key "lava_level" in worldgen/noise_settings/overworld.json to set the lava level. This overrides the config setting. This key is not present in datapacks unless manually added!

--- src/main/java/ugi/mc_fix_hardcoded_lava_level/FixHardCodedLavaLevel.java
+++ src/main/java/ugi/mc_fix_hardcoded_lava_level/FixHardCodedLavaLevel.java
@@ -1,10 +1,21 @@
 package ugi.mc_fix_hardcoded_lava_level;
 
+import com.google.gson.JsonObject;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
+import net.minecraft.resources.Identifier;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
+import net.minecraft.util.GsonHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ugi.mc_fix_hardcoded_lava_level.config.Config;
 import ugi.mc_fix_hardcoded_lava_level.config.ConfigHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class FixHardCodedLavaLevel implements ModInitializer {
     public static final String MOD_ID = "mc_fix_hardcoded_lava_level";
@@ -12,10 +23,53 @@ public class FixHardCodedLavaLevel implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     private static final int DEFAULT_SEA_LEVEL = 63; // Used for debug reasons
     private static final int DEFAULT_MINIMUM_HEIGHT = -64;
+    public static final String JSON_LAVA_LEVEL_KEY = "lava_level";
+    public static Integer overworldBottomLavaLevel; // Integer Object so null can be used
+
+    ResourceManagerReloadListener lavaLevelReloadListener =resourceManager ->
+    {
+        // Iterate over all noise setting files
+        for (Identifier dimensionNoiseSetting : resourceManager.listResources("worldgen/noise_settings", path -> path.toString().endsWith(".json")).keySet())
+        {
+            // Find the overworld noise setting file
+            if (dimensionNoiseSetting.getPath().endsWith("overworld.json"))
+            {
+                // Open stream and parse via Gson library
+	            InputStream dimensionNoiseSettingStream;
+	            try
+	            {
+		            dimensionNoiseSettingStream = resourceManager.getResourceOrThrow(dimensionNoiseSetting).open();
+                    JsonObject dimensionNoiseSettingJson = GsonHelper.parse(new InputStreamReader(dimensionNoiseSettingStream, StandardCharsets.UTF_8));
+                    // Set value only if the key exists already
+                    if (dimensionNoiseSettingJson.keySet().contains(JSON_LAVA_LEVEL_KEY))
+                    {
+                        overworldBottomLavaLevel = dimensionNoiseSettingJson.get(JSON_LAVA_LEVEL_KEY).getAsInt();
+                        if (CONFIG.verbose_Mode)
+                        {
+                            LOGGER.info("Data-driven Lava Level set to y = {} for this world", overworldBottomLavaLevel);
+                        }
+                    }
+                    else
+                    {
+                        overworldBottomLavaLevel = null;
+                    }
+	            }
+	            catch (IOException e)
+	            {
+		            throw new RuntimeException(e);
+	            }
+
+            }
+        }
+    };
 
     @Override
     public void onInitialize() {
         //FixHardCodedLavaLevel.LOGGER.info("Loading Config for " + FixHardCodedLavaLevel.MOD_ID);
+
+        // Register the datapack listener, requires the Fabric API
+        ResourceLoader.get(PackType.SERVER_DATA).registerReloadListener(Identifier.fromNamespaceAndPath(MOD_ID, "resource_loader"), lavaLevelReloadListener);
+
         Config.loadConfig();
         if (CONFIG.verbose_Mode)
         {
@@ -31,11 +85,12 @@ public class FixHardCodedLavaLevel implements ModInitializer {
                 default:
                     break;
             }
-
         }
         else
         {
             LOGGER.info("Lava Level is now smarter! Thank me later :)");
         }
     }
+
+
 }
